c405281dd95e9f39f09d6f7bfe709312601a47a1
Vladyslav Chyzhevskyi <coirius@coirius.com>
2012-07-06 22:01:24 +0300

    support for Organization types (v3)

---
+++ vCardUtils/Organization.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace vCardUtils
+{
+	public class Organization
+	{
+		public string OrganizationName { get; set; }
+		public string OrganizationUnit1 { get; set; }
+		public string OrganizationUnit2 { get; set; }
+
+		public Organization()
+		{
+			OrganizationName = string.Empty;
+			OrganizationUnit1 = string.Empty;
+			OrganizationUnit2 = string.Empty;
+		}
+	}
+}
